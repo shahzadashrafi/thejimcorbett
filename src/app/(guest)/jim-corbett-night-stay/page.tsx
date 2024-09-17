48b0faf9000228212f31b94ac50241e824765138
Shahzad Ashraf Ashrafi <shahzadashrafi.1994@gmail.com>
2024-09-17 12:13:40 +0530

    pages crration

--- src/app/(guest)/jim-corbett-night-stay/page.tsx
+++ src/app/(guest)/jim-corbett-night-stay/page.tsx
@@ -1,4 +1,70 @@
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "@/components/ui/carousel"
+  
+  import Image from "next/image"
+
+  import { AspectRatio } from "@/components/ui/aspect-ratio"
+  
 export default function CorbettSafari() {
     return (
-        <h1>Jim Corbett Night Stay</h1>
+        <div className="grid grid-col-1 md:grid-cols-2">
+        <div className="relative w-full mb-10 md:mb-0 overflow-hidden hover:bg-slate-900 hover:text-white hover:opacity-80 hover:rounded">
+        <Carousel>
+            <CarouselContent>
+                <CarouselItem>
+                    <div className="w-[450px]">
+                        <AspectRatio ratio={9 / 9}>
+                            <Image  
+                                src="/hero1.png"
+                                alt="Photo by Drew Beamer"
+                                fill
+                                className="rounded-md object-cover"
+                             />
+                        </AspectRatio>
+                    </div>
+
+                </CarouselItem>
+                <CarouselItem>
+                <CarouselItem>
+                    <div className="w-[450px]">
+                        <AspectRatio ratio={9 / 9}>
+                            <Image  
+                                src="/hero1.png"
+                                alt="Photo by Drew Beamer"
+                                fill
+                                className="rounded-md object-cover"
+                             />
+                        </AspectRatio>
+                    </div>
+
+                </CarouselItem>
+                </CarouselItem>
+                <CarouselItem>
+                <CarouselItem>
+                    <div className="w-[450px]">
+                        <AspectRatio ratio={9 / 9}>
+                            <Image  
+                                src="/hero1.png"
+                                alt="Photo by Drew Beamer"
+                                fill
+                                className="rounded-md object-cover"
+                             />
+                        </AspectRatio>
+                    </div>
+
+                </CarouselItem>
+                </CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
+        </div>
+        <div>shahzad</div>
+        </div>
     )
+}
